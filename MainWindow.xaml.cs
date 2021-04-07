f96ad0843ba04181a0647b800a9f20eef5bc5568
Jd's Code Lab <jdscodelab@gmail.com>
2021-04-07 14:10:21 +0530

    Add files via upload
    - Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IValueConvertor_INotifyPropertyChanged
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel();
+        }
+    }
+
+
+
+
+
+    //So in short IValueConverter acts like a middlemen and translates a value between the source and the destination for us... like we translated our string to bool 
+    public class TextToBoolConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //here value is our value which we get write in textbox terms of yes or No...
+            if (value is string)
+                if (!string.IsNullOrEmpty(value.ToString()))
+                    return true;
+                else
+                    return false;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //and if checkbox is checked or uncheck we got to update our textbox by writing Yes or No
+            //here value is our value which we get from checkbox when checked or unchecked in terms of True & False...
+            if (value is bool)
+                if ((bool)value == true)
+                return "Yes";
+            else
+                return "No";
+            return "No";
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IValueConvertor_INotifyPropertyChanged
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        int value1;
+        public int Value1
+        {
+            get
+            {
+                return value1;
+            }
+            set
+            {
+                value1 = value;
+                //Since we have to update the value of 3rd Textbox on change of value 1 & value 2
+                RaisePropertyChanged("Sum");
+            }
+        }
+
+        int value2;
+        public int Value2
+        {
+            get
+            {
+                return value2;
+            }
+            set
+            {
+                value2 = value;
+                RaisePropertyChanged("Sum");
+            }
+        }
+
+        int sum;
+        public int Sum
+        {
+            get
+            {
+                return value1 + value2;
+            }
+            set
+            {
+                sum = value;
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
